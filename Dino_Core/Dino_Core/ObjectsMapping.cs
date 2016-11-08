c16b33ea568cdf2fbe69d7f1b6fe55af623e635a
SmirkinDino <344919926@qq.com>
2016-11-08 23:59:16 +0800

    初始化提交

---
+++ Dino_Core/Dino_Core/ObjectsMapping.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Dino_Core
+{
+
+    public static class ObjectsMapping
+    {
+        private static Dictionary<string, Dictionary<string, string>> m_GameResourceList = new Dictionary<string, Dictionary<string, string>>();
+        public static readonly string m_ResourcePath = Application.streamingAssetsPath + "/Pref_Data/";
+        public static readonly string m_RootNode = "Root";
+
+        /// <summary>
+        /// 初始化初始化游戏物体数据列表
+        /// </summary>
+        public static void Init()
+        {
+            // 清空原有表项
+            m_GameResourceList.Clear();
+
+            // 得到目录文件夹下所有的文件.xml
+            // 每一个XML文件对应一个表单字典
+            DirectoryInfo _folder = new DirectoryInfo(m_ResourcePath);
+            foreach (FileInfo _file in _folder.GetFiles("*.xml"))
+            {
+                // 添加子表项
+                Dictionary<string, string> _childList = new Dictionary<string, string>();
+                ArrayList _list = XmlHandler.ReadNodesFromFile(m_ResourcePath + _file.Name, m_RootNode);
+                foreach (BaseNode _be in _list)
+                {
+                    if (!_childList.ContainsKey(_be.Key))
+                        _childList.Add(_be.Key, _be.Value);
+                    else
+                        Debug.Log("重名表单项：" + _be.Key);
+                }
+                // 如果总表不包含子表项，则添加进总表
+                if (!m_GameResourceList.ContainsKey(_file.Name))
+                    m_GameResourceList.Add(_file.Name.Remove(_file.Name.LastIndexOf(".")), _childList);
+                else
+                    Debug.Log("重名表单：" + _file.Name);
+            }
+            int i = 0;
+            i++;
+        }
+
+        /// <summary>
+        /// 得到数据
+        /// </summary>
+        /// <param name="_childForm"></param>
+        /// <param name="_itemName"></param>
+        public static string GetPrefData(string _formName, string _itemName)
+        {
+            if (m_GameResourceList.ContainsKey(_formName))
+                if (m_GameResourceList[_formName].ContainsKey(_itemName)) return m_GameResourceList[_formName][_itemName];
+                else Debug.Log("没有找到表项" + _itemName);
+            else Debug.Log("没有找到表单" + _formName);
+            return "";
+        }
+    }
+}
---
+++ Dino_Core/Dino_Core/XmlHandler.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+
+namespace Dino_Core
+{
+
+    /// <summary>
+    /// 封装 XML 操作类
+    /// </summary>
+    public static class XmlHandler
+    {
+        /// <summary>
+        /// 创建 xml 文件并添加内容到指定节点
+        /// </summary>
+        /// <param name="_path"></param>
+        /// <returns></returns>
+        public static bool AddNodesToFile(string _path, string _rootNodeName, BaseNode[] _nodeList)
+        {
+            XmlDocument _xmlDoc = new XmlDocument();
+            if (!File.Exists(_path))
+            {
+                try
+                {
+                    // 创建根节点
+                    XmlElement _rootNode = _xmlDoc.CreateElement("Data-List");
+                    _xmlDoc.AppendChild(_rootNode);
+                    // 创建用户所要求的父节点
+                    XmlElement _userRootNode = _xmlDoc.CreateElement(_rootNodeName);
+                    _rootNode.AppendChild(_userRootNode);
+
+                    for (int i = 0; i < _nodeList.Length; i++)
+                        AddChildtoNode(_xmlDoc, _userRootNode, _nodeList[i]);
+                }
+                catch (Exception e)
+                {
+                    Debug.Log(e);
+                    return false;
+                }
+            }
+            else
+            {
+                _xmlDoc.Load(_path);
+                XmlElement _userRootNode = _xmlDoc.SelectSingleNode("Data-List/" + _rootNodeName) as XmlElement;
+                for (int i = 0; i < _nodeList.Length; i++)
+                    AddChildtoNode(_xmlDoc, _userRootNode, _nodeList[i]);
+            }
+            _xmlDoc.Save(_path);
+            return true;
+        }
+
+        /// <summary>
+        /// 读取信息从指定的 xml 文件节点
+        /// </summary>
+        /// <param name="_path"></param>
+        /// <param name="_rootNodeName"></param>
+        /// <returns></returns>
+        public static ArrayList ReadNodesFromFile(string _path, string _rootNodeName)
+        {
+            ArrayList _resultList = new ArrayList();
+
+            if (!File.Exists(_path))
+            {
+                return _resultList;
+            }
+
+            XmlDocument _xmlDoc = new XmlDocument();
+            _xmlDoc.Load(_path);
+
+            XmlNodeList _nodeList = _xmlDoc.SelectSingleNode("Data-List/" + _rootNodeName).ChildNodes;
+            foreach (XmlNode _node in _nodeList)
+            {
+                BaseNode _xmlNode = new BaseNode(_node.Name, _node.InnerText);
+                _resultList.Add(_xmlNode);
+            }
+
+            return _resultList;
+        }
+        public static BaseNode ReadNodeFromFile(string _path, string _rootNodeName)
+        {
+            if (!File.Exists(_path))
+            {
+                return null;
+            }
+
+            XmlDocument _xmlDoc = new XmlDocument();
+            _xmlDoc.Load(_path);
+
+            XmlNode _node = _xmlDoc.SelectSingleNode("Data-List/" + _rootNodeName);
+
+            if (_node != null)
+                return new BaseNode(_node.Name, _node.InnerText);
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// 添加节点到指定节点
+        /// </summary>
+        /// <param name="_targetDoc"></param>
+        /// <param name="_targetNode"></param>
+        /// <param name="_name"></param>
+        /// <param name="_content"></param>
+        private static void AddChildtoNode(XmlDocument _targetDoc, XmlElement _targetNode, BaseNode _node)
+        {
+            XmlElement _element = _targetDoc.CreateElement(_node.Key);
+            _element.InnerText = _node.Value;
+            _targetNode.AppendChild(_element);
+        }
+
+    }
+
+}
